44a664b7c39db878c18577b2f0c0596fb4e1179a
Mosaib Khan <mosaibkhan73@gmail.com>
2021-02-03 16:28:49 +0530

    f_name to m_name

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -19,7 +19,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     public function update($user, array $input)
     {
         Validator::make($input, [
-            'f_name' => ['required', 'string', 'max:255'],
+            'm_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
             'photo' => ['nullable', 'image', 'max:1024'],
         ])->validateWithBag('updateProfileInformation');
@@ -33,7 +33,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $this->updateVerifiedUser($user, $input);
         } else {
             $user->forceFill([
-                'f_name' => $input['f_name'],
+                'm_name' => $input['m_name'],
                 'email' => $input['email'],
             ])->save();
         }
@@ -49,7 +49,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     protected function updateVerifiedUser($user, array $input)
     {
         $user->forceFill([
-            'f_name' => $input['f_name'],
+            'm_name' => $input['m_name'],
             'email' => $input['email'],
             'email_verified_at' => null,
         ])->save();
